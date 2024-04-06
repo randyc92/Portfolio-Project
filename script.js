d1aca83b772703e793ecd61011c15f1a4f556c81
randyc92 <costarandy92@gmail.com>
2024-04-06 15:39:57 -0400

    night swap

---
+++ script.js
@@ -0,0 +1,6 @@
+const body = document.querySelector('body');
+const headers = document.querySelectorAll('h2');
+const about = document.querySelector('#about-me');
+const picture = document.querySelector('.picture-swap');
+
+function k(){}
