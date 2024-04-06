c7ed262080ce0260e3e3d9c2306bb6b5dff4d247
randyc92 <costarandy92@gmail.com>
2024-04-06 16:13:23 -0400

    nightorday

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const body = document.querySelector('body');
-const headers = document.querySelectorAll('h2');
-const about = document.querySelector('#about-me');
+const headers = document.querySelectorAll('.headers');
+const about = document.querySelector('.abouts');
 const picture = document.querySelector('.picture-swap');
 
-function k(){}
+function nightOrDay(){
+    if(body.classList.contains('body-day')){
+        body.classList.remove('body-day');
+        body.classList.add('body-night');
+    } else {
+        body.classList.remove('body-night');
+        body.classList.add('body-day');
+    }
+    for(let header of headers){
+        if(header.classList.contains('headers')){
+            header.classList.remove('headers-days');
+            header.classList.add('headers-nights');
+        } else {
+            header.classList.remove('headers-nights');
+            header.classList.add('headers-days');
+        }
+    }
+
+    if(about.classList.contains('about-day')){
+        about.classList.remove('about-day');
+        about.classList.add('about-night');
+    } else {
+        about.classList.remove('about-night');
+        about.classList.add('about-day');
+    }
+}
